43381fdb22d744d498382c06e4d607416360712e
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-08 11:36:08 -0700

    commit outside of client  heroku build script

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
-
 const port = process.env.PORT || 5000;
+const path = require("path");
 
-app.use("/api", (_, res) => {
-  res.json({ data: "API info here " });
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use("/api", (req, res) => {
+  res.status(200).json({ data: "API info here " });
 });
 
 app.listen(port, () => {
   console.log("Server live on", port);
 });
-
-console.log(process.env.USER);
-console.log(process.env.PORT);
-console.log(__filename);
